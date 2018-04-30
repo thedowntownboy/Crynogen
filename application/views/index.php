e460ed7476483bca018a7dee11a8e76d122745b8
lenin <sleninfens@gmail.com>
2018-05-01 00:03:40 +0100

    removed user guide

---
+++ application/views/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" class="full-height">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Crynotech</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="./assets/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="./assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!--Project Main body-->
+
+
+    <!--Main Navigation-->
+    <header>
+
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+            <a class="navbar-brand animated slideInLeft" href="index.html"><strong>Crynotech</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto animated slideInRight">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    
+    
+        <div class="view intro-2">
+            <div class="full-bg-img">
+                <div class="mask rgba-purple-light flex-center">
+                    <div class="container text-center white-text wow fadeInUp">
+                        <p id="pmain" class="animated slideInUp">DESIGN. CODE. CREATE.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </header>
+    <!--Main Navigation-->
+    
+    <!--Main Layout-->
+    
+    <main class="text-center py-5">
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+    
+                    
+    
+                </div>
+            </div>
+        </div>
+    
+    </main>
+
+    <!--Main Layout-->
+    
+    
+    <!-- /Start your project here-->
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="./assets/js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="./assets/js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="./assets/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="./assets/js/mdb.min.js"></script>
+</body>
+
+</html>
